6309b234c02a37c9a6bc5b877a4d45a43d2398f2
Erik Lundmark <cjiqqq@gmail.com>
2019-01-07 14:42:53 +0100

    Add flag for limiting number of results

--- main.go
+++ main.go
@@ -2,25 +2,52 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 	"net/url"
 	"os"
+	"sort"
 	"strings"
 
 	"github.com/Cjiq/ubd/data"
 	"github.com/fatih/color"
 )
 
+type context struct {
+	n int
+	h bool
+}
+
+var con *context = &context{
+	n: 3,
+	h: false,
+}
+
 var apiURL = "https://api.urbandictionary.com/v0/define?term="
 
+func init() {
+	flag.IntVar(&con.n, "n", 3, "Number of results. -n 0 returns all.")
+	flag.BoolVar(&con.h, "h", false, "Show help")
+	flag.BoolVar(&con.h, "help", false, "Show help")
+	flag.Parse()
+	if con.h {
+		fmt.Println("UrbanDictionary.com v0.0.1")
+		showExample()
+		fmt.Println("Help: ")
+		flag.PrintDefaults()
+		os.Exit(0)
+	}
+}
+
 func main() {
-	if len(os.Args) <= 1 {
+	args := flag.Args()
+	if len(args) == 0 {
 		showArgumentErr()
 		os.Exit(1)
 	}
-	searchTerm := os.Args[1]
+	searchTerm := args[0]
 	if searchTerm == "" {
 		showArgumentErr()
 		os.Exit(1)
@@ -32,22 +59,36 @@ func main() {
 	}
 	defer resp.Body.Close()
 	res, err := ioutil.ReadAll(resp.Body)
+
 	var result data.Result
 	err = json.Unmarshal(res, &result)
 	if err != nil {
 		fmt.Printf("Failed to parse json: %s", err)
 	}
+
+	sort.Slice(result.Definitions, func(i, j int) bool {
+		return result.Definitions[i].Rating > result.Definitions[j].Rating
+	})
+
 	d := color.New(color.FgWhite, color.Bold)
 	d.Printf("Term: %s\n\n", searchTerm)
+
 	for i, def := range result.Definitions {
 		trim := strings.Replace(def.Text, "^M", "", -1)
 		d.Printf("Definition #%d: ", (i + 1))
 		fmt.Printf("%s\n\n", trim)
+		if (i+1) == con.n && con.n != 0 {
+			break
+		}
 	}
 }
 
 func showArgumentErr() {
 	fmt.Println("Error: You have to enter a search term.")
-	fmt.Println("Usage: ubd <search-term>")
+	showExample()
+}
+
+func showExample() {
+	fmt.Println("Usage: [-n..] ubd <search-term>")
 	fmt.Printf("Example: ubd mansplain\n\n")
 }
